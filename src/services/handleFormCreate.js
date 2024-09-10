64ff92393d52ab2d3f3af998b79a93c52477ca02
Michael Garnica <mgarnica.oficial@gmail.com>
2024-09-10 18:44:44 -0500

    Creating the first part of the fetch to the POST method

--- src/services/handleFormCreate.js
+++ src/services/handleFormCreate.js
@@ -1,5 +1,18 @@
-export default function handleSubmit(e, name, apellido, email, password){
+export default async function handleSubmit(e, name, apellido, email, password){
     e.preventDefault();
     console.log("hola mundo desde HandleSubmit");
     console.log(`your Data is: name: ${name}, apellidos: ${apellido}, email: ${email}, password: ${password}`)
+
+    const result =  await fetch("http://localhost:8081/api/estudiantes/create", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            nombres: name,
+            apellidos: apellido,
+            email,
+            contraseña: password
+        })
+    })
 }
